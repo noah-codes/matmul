e1408c4685f8e74814d46d2114d04715180d0c29
noahz <noah-codes@users.noreply.github.com>
2016-08-22 00:29:11 -0700

    Learning how to template; matrix friend methods

--- main.cpp
+++ main.cpp
@@ -3,11 +3,25 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-  double a[6] = {1, 0, 2, 2, 3, 4};
- // Matrix<double> mA(3, 2);
- // cout << mA.get(0, 1) << endl;
- // mA.fill(a);
+template <typename T>
+using Matrix = MatrixMath::Matrix<T>;
 
+int test();
+
+int main(int argc, char* argv[]) {
+  float a[6] = {1, 0, 2, 2, 3, 4};
+  Matrix<float>* mA = new Matrix<float>(2, 2);
+  Matrix<float>* mB = new Matrix<float>(2, 2);
+  Matrix<float>* q;
+  q = MatrixMath::add<float, MatrixMath::Impl::CPU>(*mA, *mB);
+  return 0;
+}
+
+int test() {
+  float a[6] = {1,2,3,4,5,6};
+  for (int i = 0; i < 6; ++i) {
+    cout << a[i];
+  }
+  cout<<endl;
   return 0;
 }
--- matrix.h
+++ matrix.h
@@ -1,25 +1,41 @@
+#pragma once
+
 #include <cassert>
 #include <cstddef>
 #include <cstdlib>
 #include <cstring>
+#include <immintrin.h>
 
 using namespace std;
+
+namespace MatrixMath {
+
+enum Impl { CPU, CPU_SIMD, GPU };
 
 template <typename T>
 class Matrix {
  public:
   Matrix(size_t = 0, size_t = 0);
+  Matrix(const Matrix<T>& matrix);
   ~Matrix();
+
+  void fill(const T* elements);
 
   T get(size_t, size_t) const;
   void set(const T value, size_t, size_t);
 
-  void fill(const T* elements);
+  template <typename S, Impl I>
+  friend Matrix<S>* add(const Matrix<S>& a, const Matrix<S>& b);
+
+  template <typename S, Impl I>
+  friend Matrix<S>* sub(const Matrix<S>& a, const Matrix<S>& b);
 
  private:
-  T* matrix_;
+  T* data_;
   size_t rows_;
   size_t columns_;
+
+  T* matrix_alloc(size_t, size_t, size_t);
 
   void check_bounds(size_t row, size_t column) const {
     assert(row < rows_);
@@ -27,33 +43,75 @@ class Matrix {
   }
 };
 
+template <typename T, Impl I>
+Matrix<T>* add(const Matrix<T>& a, const Matrix<T>& b);
+
+template <typename T, Impl I>
+Matrix<T>* sub(const Matrix<T>& a, const Matrix<T>& b);
+
+using Md = Matrix<float>;
+using Mf = Matrix<float>;
+using Mi = Matrix<int>;
+
+template <>
+Mf* add<float, Impl::CPU>(const Md& a, const Md& b) {
+  assert(a.rows_ == b.rows_ && a.columns_ == b.columns_);
+
+  Mf* c = new Md(a.rows_, a.columns_);
+
+  for (int i = 0; i < a.rows_ * a.columns_; ++i) {
+    c->data_[i] = a.data_[i] + b.data_[i];
+  }
+
+  return c;
+}
+
 template <typename T>
 Matrix<T>::Matrix(size_t rows, size_t columns) {
   // Hardcoding 32 byte alignment for AVX for now.
   rows_ = rows;
   columns_ = columns;
-  matrix_ = (T*)aligned_alloc(32, sizeof(T) * rows_ * columns_);
-  memset(matrix_, 0, sizeof(T) * rows_ * columns_);
+  data_ = matrix_alloc(rows_, columns_, 32);
+  memset(data_, 0, sizeof(T) * rows_ * columns_);
+}
+
+template <typename T>
+Matrix<T>::Matrix(const Matrix<T>& matrix) {
+  // Hardcoding 32 byte alignment for AVX for now.
+  rows_ = matrix.rows_;
+  columns_ = matrix.columns_;
+  data_ = matrix_alloc(rows_, columns_, 32);
+  memcpy(data_, matrix.data_, sizeof(T) * rows_ * columns_);
+}
+
+template <typename T>
+T* Matrix<T>::matrix_alloc(size_t rows, size_t columns, size_t align) {
+  size_t ratio = align / sizeof(T);
+  // TODO other ratios don't work, right?
+  assert(ratio == 4 || ratio == 8);
+  size_t blocks = rows * columns;
+  blocks += ratio - blocks % ratio;
+  return (T*)aligned_alloc(align, blocks * sizeof(T));
 }
 
 template <typename T>
 Matrix<T>::~Matrix() {
-  free(matrix_);
+  free(data_);
 }
 
 template <typename T>
 T Matrix<T>::get(size_t row, size_t column) const {
   check_bounds(row, column);
-  return matrix_[row * rows_ + column];
+  return data_[row * rows_ + column];
 }
 
 template <typename T>
 void Matrix<T>::set(const T value, size_t row, size_t column) {
   check_bounds(row, column);
-  matrix_[row * rows_ + column] = value;
+  data_[row * rows_ + column] = value;
 }
 
 template <typename T>
 void Matrix<T>::fill(const T* elements) {
-  memcpy(matrix_, elements, sizeof(T) * rows_ * columns_);
+  memcpy(data_, elements, sizeof(T) * rows_ * columns_);
 }
